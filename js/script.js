9de1b04ef94dc05752bf826c4da31885635d773d
leyuav <leyu.av@gmail.com>
2024-06-09 20:02:55 +0200

    Added payment overlay

--- js/script.js
+++ js/script.js
@@ -6,3 +6,34 @@ AOS.init({
   duration: 1000,
   offset: 100,
 });
+
+document.getElementById("order-now").addEventListener("click", function () {
+  document.getElementById("overlay").style.display = "flex";
+  document.body.style.overflow = "hidden"; // Prevent scrolling
+});
+
+document.getElementById("close-button").addEventListener("click", function () {
+  document.getElementById("overlay").style.display = "none";
+  document.body.style.overflow = "hidden"; // Prevent scrolling
+});
+
+document.getElementById("overlay").addEventListener("click", function (event) {
+  if (event.target === document.getElementById("overlay")) {
+    document.getElementById("overlay").style.display = "none";
+    document.body.style.overflow = ""; // Allow scrolling
+  }
+});
+
+document.getElementById("how-to-order").addEventListener("click", function () {
+  window.open("https://www.youtube.com/", "_blank");
+});
+
+document.getElementById("learn-more").addEventListener("click", function () {
+  window.open("https://sushiuniversity.jp/basicknowledge", "_blank");
+});
+
+document
+  .getElementById("payment-button")
+  .addEventListener("click", function () {
+    window.open("https://buy.stripe.com/5kAeXo4FE0E9gN26oo", "_blank");
+  });
